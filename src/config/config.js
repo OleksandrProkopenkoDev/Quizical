c04ce2a59b4dd172aada3f179c7d8d53199f3ccb
SPro <prokopenko89@gmail.com>
2023-05-21 09:49:51 +0300

    production url

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:8080";
-// export const API_URL = "https://quizzical-rest-api-production.up.railway.app";
+// export const API_URL = "http://localhost:8080";
+export const API_URL = "https://quizzical-rest-api-production.up.railway.app";
